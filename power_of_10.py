6e6c107044ffb12667d85ee63b71222015c11f95
Hugo <h.brunet13@gmail.com>
2021-09-16 20:52:43 +0800

    complete is poswer of ten

--- power_of_10.py
+++ power_of_10.py
@@ -1,52 +1,4 @@
-# import math
-# # def is_power(x, y):
-    
-# #     if x == 1:
-# #         return True
-    
-# #     pow = 1 
-# #     while (pow < y):
-# #         pow = pow * x
-
-# #     return (pow == y)
-
-# def is_power(x, y):
-#     res1 = math.log(y) // math.log(y)
-#     res2 = math.log(y) / math.log(x)
-    
-#     return 1 if(res1 == res2) else 0
-    
-    
-
-
-# print(is_power(10, 10))
-# print(is_power(10, 1000))
-# print(is_power(10, 10000))
-# # print(is_power(2,30))
-# # print(is_power(5,25))
-
-
-# def is_power(x, y):
-#     if y == 1:
-#         return True        
-#     pow = 1
-
-#     while(pow < y):
-#         pow = pow * x
-    
-#     return pow == y
-
-# import math 
-
-# def is_power(x, y):
-
-#     res = math.log(y, x)
-
-
-#     print(res)
-#     return res.is_integer()
-
-
+import math
 
 def is_power(x, y):
     if y == 1:
@@ -56,10 +8,26 @@ def is_power(x, y):
         pow = pow * x
     return pow == y
 
+def is_power_of_10(y):
+    return math.log10(y).is_integer()
 
-print(is_power(10, 1))
-print(is_power(10, 1000))
-print(is_power(10, 10090))
+print(is_power_of_10(1))
+print(is_power_of_10(10))
+print(is_power_of_10(100))
+print(is_power_of_10(1000))
+print(is_power_of_10(10000))
+print(is_power_of_10(100000))
+print(is_power_of_10(10090))
+print(is_power_of_10(8989900))
+
+
+# print(is_power(10, 1))
+# print(is_power(10, 10))
+# print(is_power(10, 100))
+# print(is_power(10, 1000))
+# print(is_power(10, 10000))
+# print(is_power(10, 100000))
+# print(is_power(8989, 10090))
 # print(is_power(10, 1050))
 
 
