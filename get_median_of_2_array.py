66f09170cf51267069c5e67b491aac1129492474
Hugo <h.brunet13@gmail.com>
2021-09-16 23:30:48 +0800

    add get_median_of_2_array()

---
+++ get_median_of_2_array.py
@@ -0,0 +1,18 @@
+
+def get_median_2_array_bad(arr1, arr2):
+    arr3 = arr1 + arr2
+    arr3.sort()
+
+    if len(arr3) % 2 == 0:
+        mid = len(arr3) // 2
+        return (arr3[mid] + arr3[mid -1]) /2
+    else:
+        return arr3[len(arr3)//2]
+
+
+arr1 = [ -5, 3, 6, 12, 15 ]
+arr2 = [ -12, -10, -6, -3, 4, 10 ]
+
+print("Median = ", get_median_2_array_bad(arr1, arr2))
+
+
