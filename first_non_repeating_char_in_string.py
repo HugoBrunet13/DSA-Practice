d9aa33d6c4bf7fac08f5548ac1662f328dd30735
Hugo <h.brunet13@gmail.com>
2021-09-16 21:47:33 +0800

    add func get_first_non_repeating_char()

---
+++ first_non_repeating_char_in_string.py
@@ -0,0 +1,33 @@
+from collections import Counter
+import datetime
+
+def get_first_non_repeating_char_optimized(string) :
+    counter = Counter(string) # get hashmap with letter of string as key, and the number of occurence as value
+    for c in string:
+        if counter[c] == 1:
+            return c
+    return False
+
+
+def get_first_non_repeating_char(string) :
+    counter = {}
+    for c in string:
+        counter[c] = counter.get(c, 0) + 1
+    # print(counter)
+    for c in string:
+        if counter[c] == 1:
+            return c
+    return False
+
+string = "geeksforgeeks"
+
+
+start=datetime.datetime.now()
+print(get_first_non_repeating_char(string))
+end=datetime.datetime.now()
+print("exec time: %s"%(end - start))
+
+start=datetime.datetime.now()
+print(get_first_non_repeating_char_optimized(string))
+end=datetime.datetime.now()
+print("exec time: %s"%(end - start))
