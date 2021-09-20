b2f0469a935f15b132cbbab588f3f7258f32869f
Hugo <h.brunet13@gmail.com>
2021-09-20 20:25:18 +0800

    add pangram() function

---
+++ pangram.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+def missing_letters(string):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for c in alphabet:
+        if c not in string:
+            print(c)
+    
+
+import string
+  
+alphabet = set(string.ascii_lowercase)
+  
+def ispangram(string):
+    print(set(string.lower()))
+    print(alphabet)
+    return set(string.lower()) >= alphabet
+
+test = "the quick brown fox jumps over the lay dog"
+
+missing_letters(test)
+print(ispangram(test))
