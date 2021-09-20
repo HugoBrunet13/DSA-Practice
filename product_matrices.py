198fa1b03eac4ace63223989001d17ceccc3f310
Hugo <h.brunet13@gmail.com>
2021-09-20 22:05:24 +0800

    add product of 2 matrices

---
+++ product_matrices.py
@@ -0,0 +1,38 @@
+# take a 3x3 matrix
+A = [[12, 7, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+ 
+# take a 3x4 matrix
+B = [[5, 8, 1, 2],
+    [6, 7, 3, 0],
+    [4, 5, 9, 1]]
+ 
+
+
+
+def product_matrice(A, B) :
+    if len(A[0]) != len(B):
+        return False
+
+    for A_row in A:
+        row = []
+        for B_col in zip(*B):
+            sum = 0
+            for a, b in zip(A_row, B_col):
+                sum += a * b
+            row.append(sum)
+        print(row)
+
+    return True
+
+    
+print(product_matrice(A, B))
+
+# # result will be 3x4
+# result = [[sum(a * b for a, b in zip(A_row, B_col))
+#                         for B_col in zip(*B)]
+#                                 for A_row in A]
+ 
+# for r in result:
+#     print(r)
