ec9b84a4add5c14b4eef5cc3d0091aab843fa446
Hugo <h.brunet13@gmail.com>
2021-09-20 22:05:42 +0800

    add min distance between 2 words

---
+++ min_distance_2_words_of_string.py
@@ -0,0 +1,45 @@
+def distance(string, w1, w2):
+    if w1 == w2:
+        return 0
+
+    string = string.split() # put string into list of words
+
+     # assume total length of the string as
+    # minimum distance
+    min_dist = len(string)+1
+
+    # get first place of first occurence
+    first_pos = None
+    for i in range(0, len(string)):
+        if string[i] == w1 or string[i] == w2:
+            first_pos = i
+            break 
+    if first_pos is None:
+        return False
+
+    # travers string starting from first occurence:
+    while (i < len(string)):
+        print("i: %s, first_pos: %s" %(i, first_pos))   
+        if string[i] == w1 or string[i] == w2:
+
+            if (string[first_pos] != string[i] and (i - first_pos) < min_dist):
+                print(string[i])
+                min_dist = i - first_pos - 1
+                first_pos = i
+            else:
+                print("in else: ", string[i])
+                first_pos = i
+        i += 1
+
+    return min_dist
+
+
+s = "geeks for geeks contribute test geeks tesh blbabla practice bla bla bla geeks test7 practice"
+w1 = "geeks"
+w2 = "practice"
+print(distance(s, w1, w2))
+
+
+
+
+    
